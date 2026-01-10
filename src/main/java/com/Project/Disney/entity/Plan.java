f773664ac45c301b438e7863b3eaf75642a6701f
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-10 19:05:49 -0300

    feat: create a plan entity

--- src/main/java/com/Project/Disney/entity/Plan.java
+++ src/main/java/com/Project/Disney/entity/Plan.java
@@ -1,4 +1,26 @@
 package com.Project.Disney.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "id")
 public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotNull
+    private BigDecimal price;
 }
