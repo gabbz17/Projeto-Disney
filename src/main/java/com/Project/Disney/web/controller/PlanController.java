8136d87e65aba9d4554dfbf5ddc038e918ed83ed
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-16 19:37:28 -0300

    feat: add a method findAll in controller

--- src/main/java/com/Project/Disney/web/controller/PlanController.java
+++ src/main/java/com/Project/Disney/web/controller/PlanController.java
@@ -3,10 +3,9 @@ package com.Project.Disney.web.controller;
 import com.Project.Disney.entity.Plan;
 import com.Project.Disney.service.PlanService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/plan")
@@ -22,5 +21,11 @@ public class PlanController {
     public ResponseEntity<Plan> create(@RequestBody Plan plan) {
         Plan plan_created = service.create(plan);
         return ResponseEntity.ok(plan_created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Plan>> findAll() {
+        List<Plan> get_all = service.findAll();
+        return ResponseEntity.ok(get_all);
     }
 }
