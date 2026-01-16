fd19e9c086e4fa43e50abfe6824a8b28f751be95
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-16 19:30:05 -0300

    feat: add a method create for plan

--- src/main/java/com/Project/Disney/service/PlanService.java
+++ src/main/java/com/Project/Disney/service/PlanService.java
@@ -1,10 +1,19 @@
 package com.Project.Disney.service;
 
 import com.Project.Disney.entity.Plan;
+import com.Project.Disney.repository.PlanRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlanService {
 
+    public final PlanRepository repository;
 
+    public PlanService(PlanRepository repository) {
+        this.repository = repository;
+    }
+
+    public Plan create(Plan plan) {
+        return repository.save(plan);
+    }
 }
