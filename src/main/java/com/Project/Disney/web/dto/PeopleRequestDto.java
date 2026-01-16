6599e090ecd6ba91786bdeef316cff0fa00b73fb
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-16 20:09:58 -0300

    feat: create a dto to request people

--- src/main/java/com/Project/Disney/web/dto/PeopleRequestDto.java
+++ src/main/java/com/Project/Disney/web/dto/PeopleRequestDto.java
@@ -1,4 +1,29 @@
 package com.Project.Disney.web.dto;
 
-public record PeopleRequestDto() {
+import com.Project.Disney.entity.Plan;
+import com.Project.Disney.entity.more.Active;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record PeopleRequestDto(
+
+        @NotBlank
+        String name,
+        @NotBlank
+        @Column(unique = true)
+        @Size(min = 11, max = 11, message = "Informe um valor de CPF válido!")
+        String cpf,
+        @NotBlank
+        @Column(unique = true)
+        @Email
+        String email,
+        @NotNull
+        @Enumerated(EnumType.STRING)
+        Active active,
+        @NotNull
+        Long plan
+) {
 }
