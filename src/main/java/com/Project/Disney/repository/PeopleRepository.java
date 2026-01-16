470f45b9f1aed11459ea1be048293d9e57c85db1
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-16 20:06:22 -0300

    feat: create a repository class for people

--- src/main/java/com/Project/Disney/repository/PeopleRepository.java
+++ src/main/java/com/Project/Disney/repository/PeopleRepository.java
@@ -1,4 +1,9 @@
 package com.Project.Disney.repository;
 
-public interface PeopleRepository {
+import com.Project.Disney.entity.People;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<People, Long> {
 }
