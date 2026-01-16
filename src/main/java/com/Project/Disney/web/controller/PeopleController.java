1335528972998eacf02a859875d3b592fcbbc419
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-16 20:10:35 -0300

    feat: add a method create in controller

--- src/main/java/com/Project/Disney/web/controller/PeopleController.java
+++ src/main/java/com/Project/Disney/web/controller/PeopleController.java
@@ -1,4 +1,27 @@
 package com.Project.Disney.web.controller;
 
+import com.Project.Disney.entity.People;
+import com.Project.Disney.service.PeopleService;
+import com.Project.Disney.web.dto.PeopleRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/people")
 public class PeopleController {
+
+    public final PeopleService service;
+
+    public PeopleController(PeopleService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody PeopleRequestDto dto) {
+        People created = service.create(dto);
+        return ResponseEntity.status(201).body("Usuário criado com sucesso!");
+    }
 }
