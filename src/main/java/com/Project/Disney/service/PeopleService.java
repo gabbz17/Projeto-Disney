89ae2eb32b219a43c4442c627f7af08f82f88b7f
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-16 20:08:13 -0300

    feat: add a method create in peopleService

--- src/main/java/com/Project/Disney/service/PeopleService.java
+++ src/main/java/com/Project/Disney/service/PeopleService.java
@@ -1,4 +1,32 @@
 package com.Project.Disney.service;
 
+import com.Project.Disney.entity.People;
+import com.Project.Disney.entity.Plan;
+import com.Project.Disney.repository.PeopleRepository;
+import com.Project.Disney.web.dto.PeopleRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PeopleService {
+
+    public final PeopleRepository repository;
+    public final PlanService planService;
+
+    public PeopleService(PeopleRepository repository, PlanService planService) {
+        this.repository = repository;
+        this.planService = planService;
+    }
+
+    public People create(PeopleRequestDto dto) {
+        People people = new People();
+        Plan plan = planService.findById(dto.plan());
+
+        people.setName(dto.name());
+        people.setCpf(dto.cpf());
+        people.setEmail(dto.email());
+        people.setActive(dto.active());
+        people.setPlan(plan);
+
+        return repository.save(people);
+    }
 }
