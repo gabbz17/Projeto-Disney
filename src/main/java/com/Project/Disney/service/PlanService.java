332baae59a41dfd6df0b7afffbd5db8208810ddc
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-18 11:09:08 -0300

    refactor: complement with exception

--- src/main/java/com/Project/Disney/service/PlanService.java
+++ src/main/java/com/Project/Disney/service/PlanService.java
@@ -1,6 +1,9 @@
 package com.Project.Disney.service;
 
 import com.Project.Disney.entity.Plan;
+import com.Project.Disney.exception.IdNotFoundException;
+import com.Project.Disney.exception.ListNotFoundException;
+import com.Project.Disney.exception.NameUniqueException;
 import com.Project.Disney.repository.PlanRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +19,25 @@ public class PlanService {
     }
 
     public Plan create(Plan plan) {
-        return repository.save(plan);
+        try {
+
+            return repository.save(plan);
+        } catch (org.springframework.dao.DataIntegrityViolationException e) {
+
+            throw new NameUniqueException(String.format("Plano com o nome (%s), já cadastrado!", plan.getName()));
+        }
     }
 
     public List<Plan> findAll() {
+        if (repository.findAll().isEmpty()) {
+            throw  new ListNotFoundException("Produtos não encontrados!");
+        }
+
         return repository.findAll();
     }
 
     public Plan findById(Long id) {
-        return repository.findById(id).get();
+        return repository.findById(id).orElseThrow(() ->
+                new IdNotFoundException(String.format("Plano com o id (%d), não encontrado!", id)));
     }
 }
