818f13048e25dd044f672f44e3c62ba2cb1c6b71
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-18 17:13:46 -0300

    feat: add a topic of kafka

--- src/main/java/com/Project/Disney/web/controller/PeopleController.java
+++ src/main/java/com/Project/Disney/web/controller/PeopleController.java
@@ -2,10 +2,13 @@ package com.Project.Disney.web.controller;
 
 import com.Project.Disney.entity.People;
 import com.Project.Disney.service.PeopleService;
+import com.Project.Disney.web.dto.EmailRequestDto;
 import com.Project.Disney.web.dto.PeopleRequestDto;
 import com.Project.Disney.web.dto.PeopleResponseDto;
 import com.Project.Disney.web.mapper.PeopleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +16,9 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/people")
 public class PeopleController {
+
+    @Autowired
+    KafkaTemplate<String, Object> kafkaTemplate;
 
     public final PeopleService service;
 
@@ -22,7 +28,9 @@ public class PeopleController {
 
     @PostMapping
     public ResponseEntity<String> create(@RequestBody PeopleRequestDto dto) {
-        People created = service.create(dto);
+        service.create(dto);
+        EmailRequestDto requestDto = new EmailRequestDto(dto.name(), dto.email());
+        kafkaTemplate.send("email-Disney-topic", requestDto);
         return ResponseEntity.status(201).body("Usuário criado com sucesso!");
     }
 
