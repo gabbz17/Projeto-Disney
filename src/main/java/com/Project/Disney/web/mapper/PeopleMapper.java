3e8d97b7567fe32f9c6d48f32a44cb5a8685390f
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-18 10:44:38 -0300

    feat: create a mapper class to filter

--- src/main/java/com/Project/Disney/web/mapper/PeopleMapper.java
+++ src/main/java/com/Project/Disney/web/mapper/PeopleMapper.java
@@ -1,4 +1,11 @@
 package com.Project.Disney.web.mapper;
 
+import com.Project.Disney.entity.People;
+import com.Project.Disney.web.dto.PeopleResponseDto;
+
 public class PeopleMapper {
+
+    public static PeopleResponseDto toDto(People people) {
+        return new PeopleResponseDto(people.getId(), people.getName(), people.getActive(), people.getPlan());
+    }
 }
